1ae324a0ccd07da025559daa8d927b8385d13566
ilker yagci <ilkerya@gmail.com>
2026-02-20 11:58:13 +0100

    fixes

--- Defs.h
+++ Defs.h
@@ -1,5 +1,37 @@
 /*
 
+First Time
+git init
+git add README.md
+git add Branch main
+
+GitHub Bash Commands
+Adding new file
+https://help.github.com/articles/adding-a-file-to-a-repository-using-the-command-line/#platform-windows
+
+git add .
+
+git commit -m "Add existing file"
+// For Common git
+git push https://github.com/ilkerya/Baracuda master
+
+
+In case in the first push gives error use below command
+git remote add origin remote repository URL
+git push origin master
+git push --force https://github.com/ilkerya/Baracuda master --force
+
+To create a branch:
+$ git branch <branch_name> 
+To switch to that branch:
+$ git checkout <same_branch_name> 
+To do the above operations in one line, it will create and switch branch:
+$ git checkout -b <new_branch> 
+For example, say you want to create a branch named `learning_git`
+$ git branch learning_git 
+$ git checkout learning_git 
+or
+$ git checkout -b learning_git 
 I2C scanner. Scanning ...
 Found address: 26 (0x1A)
 Found address: 56 (0x38) //aht20
