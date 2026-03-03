b39d31d3f8dc2a98fa5c92b41d07020d185b3d2e
Ilker Yagci <ilker.yagci@se.abb.com>
2026-03-03 16:05:50 +0100

    branch

---
+++ Fragrance.ino
@@ -0,0 +1,201 @@
+// git add .
+// git commit -m "Time Stamp"
+// https://github.com/ilkerya/Scent_Diffuser-Firmware.git
+
+/*
+ Repeat timer example
+
+ This example shows how to use hardware timer in ESP32. The timer calls onTimer
+ function every second. The timer can be stopped with button attached to PIN 0
+ (IO0).
+ This example code is in the public domain.
+
+ ESP WROOM 32E ->
+ https://documentation.espressif.com/esp32-wroom-32e_esp32-wroom-32ue_datasheet_en.pdf
+
+ ESP32-D0WD-V3 chip or ESP32-D0WDR2-V3 chip
+https://documentation.espressif.com/esp32_datasheet_en.pdf
+
+
+ */
+// Stop button is attached to PIN 0 (IO0)
+
+
+#include <Preferences.h>
+#include <EEPROM.h>
+Preferences NV_Mem;
+
+#include <Wire.h>
+//#include <RTClib.h>
+//#include <Adafruit_Si7021.h>
+#include <esp_task_wdt.h>
+
+//#define LED_OUT
+
+#include  "Defs.h"
+#include "driver/rtc_io.h"
+
+#include "Variables.h"
+#include "DAQ.h"
+#include "Sensors.h"
+#include "Functions.h"
+
+
+//ESP32 Update Link from preferences
+// https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+
+void setup() {
+    WatchdogTimer_Set();
+  Init_IO();
+  Serial.begin(115200);
+ // EEPROM.begin(EEPROM_SIZE);
+
+  print_wakeup_reason();
+
+  esp_reset_reason_t reason = esp_reset_reason();
+  Serial.print("Reset reason: ");
+  Serial.println(reason); // Prints integer value
+
+  Wire.begin (SDA, SCL);   // sda= GPIO_18 /scl= GPIO_19
+//  Found address: 26 (0x1A)
+// Found address: 56 (0x38)
+// Found address: 83 (0x53)
+ // Scanner ();
+   //analogWrite(ledPin, dutyCycle);
+ // print_wakeup_reason();
+
+  Key.Task = OFF;
+
+  Init_TempHSensors();     
+  Init_Light_Sensor();
+  Init_TVoc();
+  //Mode = TEST_FRAG;
+
+
+
+    NV_Mem.begin("NV_Mem_Mode",false );
+    NV_Mem.begin("NV_Mem_Fan_High",false );
+    NV_Mem.begin("NV_Mem_Fan_Mid",false );
+    NV_Mem.begin("NV_Mem_Fan_Low",false );
+
+    uint8_t Val = NV_Mem.getUChar("NV_Mem_Mode", 0);
+  if(!((Val == DEVICE_OFF) || (Val == FAN_HIGH)|| (Val == FAN_MID)|| (Val == FAN_LOW))){  
+     Mode = DEVICE_OFF;// write default
+      NV_Mem.putUChar("NV_Mem_Mode", Mode);
+      Serial.print(F("Mode")) ;   
+  }
+  else Mode = Val;
+  Val = NV_Mem.getUChar("NV_Mem_Fan_High", 0);
+  if(!((Val > 15) && (Val < 99))){  
+     Fan.HighSpeed = 80; // write default
+      NV_Mem.putUChar("NV_Mem_Fan_High", Fan.HighSpeed);
+      Serial.print(F("Fan.HighSpeed")) ;    
+  }
+  else Fan.HighSpeed = Val;
+  Val = NV_Mem.getUChar("NV_Mem_Fan_Mid", 0);
+  if(!((Val > 15) && (Val < 99))){  
+     Fan.MidSpeed = 60; // write default
+      NV_Mem.putUChar("NV_Mem_Fan_Mid", Fan.MidSpeed);
+      Serial.print(F("Fan.MidSpeed")) ;    
+  }
+  else Fan.MidSpeed = Val;
+  Val = NV_Mem.getUChar("NV_Mem_Fan_Low", 0);
+  if(!((Val > 15) && (Val < 99))){  
+     Fan.LowSpeed = 40; // write default
+      NV_Mem.putUChar("NV_Mem_Fan_Low", Fan.LowSpeed);
+      Serial.print(F("Fan.LowSpeed")) ;    
+  }
+  else Fan.LowSpeed = Val;
+
+ // Read_NV_Memory();
+                    
+  Interrupt_Set();
+}
+
+  void Rpm_Calculate(){
+      Fan.Rpm = 2 * (Fan.Pulse_Low_Latch + Fan.Pulse_High_Latch)+1;
+     // if(Fan.Rpm !=0)  Fan.Rpm = 6000000 / Fan.Rpm;  // be careful for divide by 0 errror    
+    if(!Fan.Error) Fan.Rpm =  6000000 /Fan.Rpm; 
+    else Fan.Rpm = 0;
+  }
+
+uint8_t CounterPI;
+
+
+uint32_t isrCount = 0, isrTime = 0;
+void loop() {
+   esp_task_wdt_reset(); // 10 seconds
+  // If Timer has fired
+  if (xSemaphoreTake(timerSemaphore, 0) == pdTRUE){
+    //uint32_t isrCount = 0, isrTime = 0;
+    // Read the interrupt count and time
+    portENTER_CRITICAL(&timerMux);
+    isrCount = isrCounter;
+    isrTime = lastIsrAt;
+    portEXIT_CRITICAL(&timerMux);
+  }
+  Execute_Serial_Commands();
+
+  if(LOOP_20mSec){
+     LOOP_20mSec = OFF;
+      Mode_Select(); 
+      Led_Control();
+      ledcWrite(FAN_PWM, 255-((Fan.DutyCycle*255)/100) ); 
+      Rpm_Calculate();
+  }
+  if(LOOP_1Second){
+     LOOP_1Second = OFF;
+    if(Key.Task) {
+      Key.Task = OFF;
+      NV_Mem.putUChar("NV_Mem_Mode", Mode);
+    }
+     //   ((Fan.DutyCycle*255)/100)  = 255-PWM_Counter;
+   //     PWM_Counter = 255-((Fan.DutyCycle*255)/100) 
+     // digitalWrite(SENSOR_3V_POWER, SENSOR_3V_ENABLE); 
+    digitalWrite(SENSOR_3V_POWER, SENSOR_3V_ENABLE); 
+    Read_Temperature();
+    Read_TVoc();
+    Read_Light();
+
+    PC_Serial_Mode = OFF;
+    if(PC_Serial_Mode)
+        DAQ_Send_Data(LOOP_BASED); 
+    else{
+     // Serial.print(F("Mode:")); 
+      if(Mode == DEVICE_OFF) Serial.print(F("Off ")); 
+      if(Mode == FAN_HIGH)   Serial.print(F("High ")); 
+      if(Mode == FAN_MID) Serial.print(F("Mid ")); 
+      if(Mode == FAN_LOW)    Serial.print(F("Low "));      
+     //  Serial.print("Fan: "); 
+       Serial.print(Fan.Rpm); Serial.print(F("Rpm-%"));Serial.print(Fan.DutyCycle); Serial.print(F("DC ")) ;     
+      Serial.print(Values.Temperature,1);Serial.print(F("°C %")); Serial.print(Values.Humidity,0);   
+     // Serial.print(" TVOC: ");
+     Serial.print(F("rh "));
+     if(Values.TVoc_Error== ON)Serial.print(F("  "));
+     else Serial.print(Values.TVoc);
+    Serial.print(F(" ppb "));
+      Serial.print(Values.Lux,1); Serial.print(F("Lux / %Set:")); 	
+
+    Serial.print(Fan.LowSpeed);Serial.print(F("/")); Serial.print(Fan.MidSpeed);Serial.print(F("/")); Serial.print(Fan.HighSpeed);
+
+
+    //  Serial.print("   Int:");Serial.print(lastIsrAt_Diff);Serial.print("us "); 
+   //   Serial.print(" Key: "); 	Serial.print(Key.Key1_Rel); 
+    //  Serial.print(" Key: ");
+    //  Serial.print( Key.Key1);
+    //  Serial.print( Key.Key1_Rel);
+    //  Serial.print(" Status: ");
+    //  Serial.print( Key.Status);
+   //   Serial.print("  KeyTimer:"); Serial.print( Key.TimerPress); 
+
+    //    Serial.print(F("  Ver:"));
+  // Serial.print(__DATE__", "__TIME__","__VERSION__); 
+
+      Serial.println(""); 
+
+    }  
+  }
+}
+
+
+
---
+++ Functions.h
@@ -0,0 +1,490 @@
+#include <array>
+uint16_t Led_Que = 0;
+hw_timer_t * timer = NULL;
+volatile SemaphoreHandle_t timerSemaphore;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+volatile uint32_t isrCounter = 0;
+volatile uint32_t lastIsrAt = 0;
+volatile uint32_t lastIsrAt_Prev = 0;
+volatile uint32_t lastIsrAt_Diff= 0;
+
+#define WDT_TIMEOUT 10000 // 10000mS = 10 second... ->
+#define CONFIG_FREERTOS_NUMBER_OF_CORES 1
+void WatchdogTimer_Set(){
+  esp_task_wdt_config_t twdt_config =
+    {
+        .timeout_ms = WDT_TIMEOUT,
+        .idle_core_mask = 0,    // Bitmask of cores
+        .trigger_panic = true,
+    };
+  esp_task_wdt_deinit(); //wdt is enabled by default, so we need to 'deinit' it first
+  esp_task_wdt_init(&twdt_config); //enable panic so ESP32 restarts
+  esp_task_wdt_add(NULL); //add current thread to WDT watch
+}
+
+void ARDUINO_ISR_ATTR onTimer(){
+  // Increment the counter and set the time of ISR
+  portENTER_CRITICAL_ISR(&timerMux);
+  isrCounter = isrCounter + 1;
+ // lastIsrAt = millis();
+  lastIsrAt = micros();
+  lastIsrAt_Diff = lastIsrAt - lastIsrAt_Prev;
+  lastIsrAt_Prev = lastIsrAt;
+  portEXIT_CRITICAL_ISR(&timerMux);
+  // Give a semaphore that we can check in the loop
+  xSemaphoreGiveFromISR(timerSemaphore, NULL);
+  // It is safe to use digitalRead/Write here if you want to toggle an output
+   //1 msec
+
+
+  Loop_1SecCounter++;
+  if(Loop_1SecCounter >= 100000){
+    Loop_1SecCounter = 0;
+    LOOP_1Second = ON;
+  }
+  Loop_20mSecCounter++;
+  if(Loop_20mSecCounter >= 2000){
+    Loop_20mSecCounter = 0;
+    LOOP_20mSec = ON;
+   Key_Functions_Digital();
+  }
+#define TACHO_ERROR 30000 // low tahn 100 rpm
+   if(digitalRead(FAN_FEEDBACK)){
+    if(Fan.Pulse_Low > 0){
+        Fan.Pulse_Low_Latch = Fan.Pulse_Low;
+        Fan.Pulse_Low = 0;
+        Fan.Error = OFF;
+    }
+    Fan.Pulse_High++;
+    if(Fan.Pulse_High > TACHO_ERROR){ //error
+      Fan.Error = ON;
+      //Fan.Pulse_High_Latch = TACHO_ERROR;
+     // Fan.Pulse_Low_Latch = TACHO_ERROR;
+    }
+   }
+   else {
+    if(Fan.Pulse_High > 0){
+        Fan.Pulse_High_Latch = Fan.Pulse_High;
+        Fan.Pulse_High = 0;
+        Fan.Error = OFF;
+    }
+    Fan.Pulse_Low++;
+    if(Fan.Pulse_Low > TACHO_ERROR){ //error
+      Fan.Error = ON;
+     // Fan.Pulse_High_Latch = TACHO_ERROR;
+     // Fan.Pulse_Low_Latch = TACHO_ERROR;
+    }
+   }
+
+}
+void Interrupt_Set(void){
+  // Create semaphore to inform us when the timer has fired
+  timerSemaphore = xSemaphoreCreateBinary();
+  // Set timer frequency to 1Mhz resolution
+  timer = timerBegin(1000000);
+  // Attach onTimer function to our timer.
+  timerAttachInterrupt(timer, &onTimer);
+  // Set alarm to call onTimer function every second (value in microseconds).
+  // Repeat the alarm (third parameter) with unlimited count = 0 (fourth parameter).
+ // timerAlarm(timer, TIME_USEC, true, 0);// 100000 usec => 1000 msec => 1 sec
+  #define TIME_1MSEC 1000 // 1 msec resolution
+  #define TIME_100USEC 100 // 1 msec resolution
+  #define TIME_10USEC 10 // 1 msec resolution
+  #define TIME_1USEC 1 // 1 msec resolution
+
+  timerAlarm(timer, TIME_10USEC, true, 0);// 1000 usec => 1 msec
+ // pinMode(BTN_STOP_ALARM, INPUT);
+}
+/*
+void SetDutyCycle(uint8_t DC){
+  //if(Mode == TEST_FRAG)
+  Fan.DutyCycle = DC;
+            EEPROM.write(EPPROM_ADR_SPEED_HIGH, Fan.HighSpeed); 
+            EEPROM.commit();  
+
+  Key.Task = ON;
+}
+*/
+void Print_DC_Error(){
+    Serial.println(F("Not Set Min %15 /Max %99"));  
+}
+
+
+
+
+ void Execute_Serial_Commands(){
+  uint32_t DutyCycleTemp;
+    while (Serial.available()) {
+    char incomingChar = Serial.read();  // Read each character from the buffer
+    //  static const char LOG_5MSEC[]   PROGMEM = "  5 mS"; //12
+    if (incomingChar == '\n') {  // Check if the user pressed Enter (new line character)
+      // Print the message
+    // Serial.print("Rx: ");
+     // Serial.println(receivedMessage);
+      Serial.print("Message Rx   ");
+      if (receivedMessage.substring(0,9) == "SpeedHigh") {
+        Serial.print(F("SpeedHigh Fan Set To "));
+        DutyCycleTemp = (receivedMessage.substring(10,13)).toInt(); 
+        if((DutyCycleTemp > 15) && (DutyCycleTemp < 99)){    
+          Fan.HighSpeed = (uint8_t)DutyCycleTemp;
+          if(Mode == FAN_HIGH)Fan.DutyCycle =Fan.HighSpeed; 
+          Serial.print(F("HighSpeed: ")); 
+          //  EEPROM.write(EPPROM_ADR_SPEED_HIGH, Fan.HighSpeed); 
+          //  EEPROM.commit(); 
+             NV_Mem.putUChar("NV_Mem_Fan_High", Fan.HighSpeed);
+
+             Serial.print(NV_Mem.getUChar("NV_Mem_Fan_High", 0)); 
+                  
+              Serial.println(F("% Duty Cycle"));      
+            
+        }
+        else Print_DC_Error(); 
+      }
+      if (receivedMessage.substring(0,8) == "SpeedMid") {
+        Serial.print(F("SpeedMid Fan Set To "));
+        DutyCycleTemp = (receivedMessage.substring(9,12)).toInt(); 
+        if((DutyCycleTemp > 15) && (DutyCycleTemp < 99)){    
+          Fan.MidSpeed = (uint8_t)DutyCycleTemp;
+          if(Mode == FAN_MID)Fan.DutyCycle =Fan.MidSpeed; 
+          Serial.print(Fan.MidSpeed);   
+          Serial.println(F("% Duty Cycle")); 
+         // EEPROM.write(EPPROM_ADR_SPEED_MID, Fan.MidSpeed); 
+         // EEPROM.commit(); 
+           NV_Mem.putUChar("NV_Mem_Fan_Mid", Fan.MidSpeed);
+           
+        }     
+        else Print_DC_Error(); 
+
+      } //       Serial.print(F("  Compiled:"));
+       if (receivedMessage.substring(0,8) == "SpeedLow") {
+        Serial.print(F("SpeedLow Fan Set To "));
+        DutyCycleTemp = (receivedMessage.substring(9,12)).toInt(); 
+        if((DutyCycleTemp > 15) && (DutyCycleTemp < 99)){          
+          Fan.LowSpeed = (uint8_t)DutyCycleTemp;
+          if(Mode == FAN_LOW)Fan.DutyCycle =Fan.LowSpeed; 
+          Serial.print(Fan.LowSpeed);   
+          Serial.println(F("% Duty Cycle"));  
+          // EEPROM.write(EPPROM_ADR_SPEED_LOW, Fan.LowSpeed);   
+          // EEPROM.commit();   
+            NV_Mem.putUChar("NV_Mem_Fan_Low", Fan.LowSpeed);
+       
+        }     
+        else Print_DC_Error();        
+      }     
+
+      receivedMessage = "";
+    } else {
+      // Append the character to the message string
+      receivedMessage += incomingChar;
+    }
+  }
+}
+void Key_Function(){
+ //   Mode++;
+ //   if(Mode > FAN_HIGH)Mode = DEVICE_OFF;
+  
+  }
+
+void Mode_Select() {
+  //  if(!Key.Task) return;
+  //  Key.Task = OFF;
+
+      if(Mode == DEVICE_OFF) {
+        Fan.DutyCycle = 1;   digitalWrite(BOOST_CONV_POWER, OFF);
+        }
+      if(Mode == FAN_HIGH)   {
+        Fan.DutyCycle =Fan.HighSpeed; digitalWrite(BOOST_CONV_POWER, ON);
+        }
+      if(Mode == FAN_MID) {
+        Fan.DutyCycle = Fan.MidSpeed; digitalWrite(BOOST_CONV_POWER, ON);
+        }
+      if(Mode == FAN_LOW)    {
+        Fan.DutyCycle =Fan.LowSpeed;  digitalWrite(BOOST_CONV_POWER, ON);
+        }
+   //     EEPROM.write(EPPROM_ADR_MODE, Mode); // write default
+    //    EEPROM.commit();
+       //   NV_Mem.putUChar("NV_Mem_Mode", Mode);
+
+       // Led_Control();
+ }
+
+
+void Key_Functions_Digital(void) {
+
+  Key.Key1 = digitalRead(KEY); //release 1
+    if (!Key.Key1_Rel && Key.Key1) {  // default
+    Key.TimerPress = 0;
+     return;
+  }
+
+  if (!Key.Key1_Rel && !Key.Key1) {  // key1 pressedd   Key.Key1_Rel = 0 normally
+    Key.Key1_Rel = 1;//    0 && 0   rel && press
+    Key.TimerPress ++;
+    return;
+  }
+  if (Key.Key1_Rel && !Key.Key1) {  // still pressed
+ 
+    Key.TimerPress ++;
+    if(Key.TimerPress > 350)ESP.restart(); //20ms*350 = 7000mS 7 sec
+  }
+
+  if (Key.Key1_Rel && Key.Key1) {  // key released job done
+    Key.Key1_Rel = 0;
+     Mode++;
+    if(Mode > 3)Mode = 0;
+       Key.Task = ON;
+      // NV_Mem.putUChar("NV_Mem_Mode", Mode);
+    //   Key.EEPROM_Task = ON;
+   }
+}
+
+
+void  Init_IO(void){
+
+  pinMode(BAT_CHARGE, INPUT);
+  pinMode(BAT_STANDBYE, INPUT);
+  pinMode(FAN_FEEDBACK, INPUT);
+  pinMode(KEY, INPUT);
+  pinMode (KEY, INPUT_PULLUP);
+
+  pinMode(FAN_PWM, OUTPUT);
+  digitalWrite(FAN_PWM, OFF);
+
+   ledcAttach(FAN_PWM, 25000, 8);
+
+  pinMode(BOOST_CONV_POWER, OUTPUT);
+  digitalWrite(BOOST_CONV_POWER, ON);
+
+  pinMode(LED_CANDLE, OUTPUT);
+  digitalWrite(LED_CANDLE, ON);
+
+  pinMode(SENSOR_3V_POWER, OUTPUT);
+       digitalWrite(SENSOR_3V_POWER, SENSOR_3V_DISABLE);
+  //digitalWrite(SENSOR_3V_POWER, SENSOR_3V_ENABLE);
+
+  pinMode(LED_BLUE, OUTPUT);
+   digitalWrite(LED_BLUE, OFF);
+
+   pinMode(LED_GREEN, OUTPUT);
+   digitalWrite(LED_GREEN, OFF);
+
+   pinMode(LED_RED, OUTPUT);
+   digitalWrite(LED_RED, OFF);
+
+  // analogSetWidth(11);               // 11Bit resolution
+
+  //analogSetAttenuation(ADC_0db);
+}
+void Led_Control(void){
+  if(Mode == DEVICE_OFF) {
+    digitalWrite(LED_GREEN, OFF);
+    digitalWrite(LED_BLUE, OFF);
+    digitalWrite(LED_RED, OFF);
+  }
+  if(Mode == FAN_HIGH)   {
+    digitalWrite(LED_GREEN, ON);
+    digitalWrite(LED_BLUE, ON);
+    digitalWrite(LED_RED, ON);
+  }
+  if(Mode == FAN_MID) {
+    digitalWrite(LED_GREEN, OFF);
+    digitalWrite(LED_BLUE, ON);
+    digitalWrite(LED_RED, OFF);
+  }
+  if(Mode == FAN_LOW)    {
+    digitalWrite(LED_GREEN, ON);
+    digitalWrite(LED_BLUE, OFF);
+    digitalWrite(LED_RED, OFF);
+  }
+
+ //   digitalWrite(FAN_PWM, !digitalRead(FAN_PWM));
+/*
+    switch(Led_Que){
+      case 0:  digitalWrite(LED_GREEN, OFF);
+              digitalWrite(LED_BLUE, OFF);
+              digitalWrite(LED_RED, ON);
+        break;
+      case 1:   digitalWrite(LED_GREEN, OFF);
+              digitalWrite(LED_BLUE, ON);
+               digitalWrite(LED_RED, OFF);
+        break;
+      case 2:   digitalWrite(LED_GREEN, ON);
+              digitalWrite(LED_BLUE, OFF);
+              digitalWrite(LED_RED, OFF);
+        break;
+    }
+    Led_Que++;
+    if(Led_Que > 2){ //1500 msec
+        Led_Que = 0;
+    }
+    */
+}
+  void Read_NV_Memory(){
+
+      uint8_t Val;
+
+
+
+
+
+    //  NV_Mem.putUChar("Mode8", 45);
+
+  //NV_Mem.putUChar("NV_Mem_Fan_High", Fan.HighSpeed);
+  //State8= NV_Mem.getUChar("NV_Mem_Fan_High", 0);
+
+
+  Val = NV_Mem.getUChar("NV_Mem_Mode", 0);
+  if(!((Val == DEVICE_OFF) || (Val == FAN_HIGH)|| (Val == FAN_MID)|| (Val == FAN_LOW))){  
+     Mode = DEVICE_OFF;// write default
+      NV_Mem.putUChar("NV_Mem_Mode", Mode);
+      Serial.print(F("Mode")) ;   
+  }
+  else Mode = Val;
+  Val = NV_Mem.getUChar("NV_Mem_Fan_High", 0);
+  if(!((Val > 15) && (Val < 99))){  
+     Fan.HighSpeed = 80; // write default
+      NV_Mem.putUChar("NV_Mem_Fan_High", Fan.HighSpeed);
+      Serial.print(F("Fan.HighSpeed")) ;    
+  }
+  else Fan.HighSpeed = Val;
+  Val = NV_Mem.getUChar("NV_Mem_Fan_Mid", 0);
+  if(!((Val > 15) && (Val < 99))){  
+     Fan.MidSpeed = 60; // write default
+      NV_Mem.putUChar("NV_Mem_Fan_Mid", Fan.MidSpeed);
+      Serial.print(F("Fan.MidSpeed")) ;    
+  }
+  else Fan.MidSpeed = Val;
+  Val = NV_Mem.getUChar("NV_Mem_Fan_Low", 0);
+  if(!((Val > 15) && (Val < 99))){  
+     Fan.LowSpeed = 40; // write default
+      NV_Mem.putUChar("NV_Mem_Fan_Low", Fan.LowSpeed);
+      Serial.print(F("Fan.LowSpeed")) ;    
+  }
+  else Fan.LowSpeed = Val;
+
+
+/*
+
+  Val = EEPROM.read(EPPROM_ADR_MODE);
+  if(!((Val == DEVICE_OFF) || (Val == FAN_HIGH)|| (Val == FAN_MID)|| (Val == FAN_LOW))){  
+     Mode = DEVICE_OFF;// write default
+      EEPROM.write(EPPROM_ADR_MODE, Mode); // write default
+      EEPROM.commit();  
+            Serial.print(F("Mode")) ;   
+  }
+  else Mode = Val;
+
+  Val = EEPROM.read(EPPROM_ADR_SPEED_HIGH);
+  if(!((Val > 15) && (Val < 99))){  
+     Fan.HighSpeed = 80; // write default
+      EEPROM.write(EPPROM_ADR_SPEED_HIGH, Fan.HighSpeed); // write default
+      EEPROM.commit();  
+      Serial.print(F("Fan.HighSpeed")) ;    
+  }
+  else Fan.HighSpeed = Val;
+
+  Val = EEPROM.read(EPPROM_ADR_SPEED_MID);
+  if(!((Val > 15) && (Val < 99))){  
+     Fan.MidSpeed = 60; // write default
+      EEPROM.write(EPPROM_ADR_SPEED_MID, Fan.MidSpeed); // write default
+      EEPROM.commit();  
+      Serial.print(F("Fan.MidSpeed")) ;   
+  }
+  else Fan.MidSpeed = Val;
+
+   Val = EEPROM.read(EPPROM_ADR_SPEED_LOW);
+  if(!((Val > 15) && (Val < 99))){  
+     Fan.LowSpeed = 40; // write default
+      EEPROM.write(EPPROM_ADR_SPEED_LOW, Fan.LowSpeed); // write default
+      EEPROM.commit();  
+      Serial.print(F("Fan.LowSpeed")) ;   
+  }
+  else Fan.LowSpeed = Val; 
+  */
+}
+
+//RTC_DATA_ATTR int bootCount = 0;
+
+void print_wakeup_reason() {
+  esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch (wakeup_reason) {
+    case ESP_SLEEP_WAKEUP_EXT0:     Serial.println("Wakeup caused by external signal using RTC_IO"); break;
+    case ESP_SLEEP_WAKEUP_EXT1:     Serial.println("Wakeup caused by external signal using RTC_CNTL"); break;
+    case ESP_SLEEP_WAKEUP_TIMER:    Serial.println("Wakeup caused by timer"); break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD: Serial.println("Wakeup caused by touchpad"); break;
+    case ESP_SLEEP_WAKEUP_ULP:      Serial.println("Wakeup caused by ULP program"); break;
+    default:                        Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeup_reason); break;
+  }
+}
+#define WAKEUP_GPIO_KEY2              GPIO_NUM_39     // Only RTC IO are allowed - ESP32 Pin example
+void Set_Sleep(void){
+  esp_sleep_enable_ext0_wakeup(WAKEUP_GPIO_KEY2, 0);  //1 = High, 0 = Low
+  /*
+  rtc_gpio_pullup_dis(WAKEUP_GPIO_KEY2);
+  rtc_gpio_pulldown_en(WAKEUP_GPIO_KEY2);
+*/
+  rtc_gpio_pullup_en(WAKEUP_GPIO_KEY2);
+  rtc_gpio_pulldown_dis(WAKEUP_GPIO_KEY2);
+
+/*
+#if USE_EXT0_WAKEUP
+  esp_sleep_enable_ext0_wakeup(WAKEUP_GPIO, 0);  //1 = High, 0 = Low
+  // Configure pullup/downs via RTCIO to tie wakeup pins to inactive level during deepsleep.
+  // EXT0 resides in the same power domain (RTC_PERIPH) as the RTC IO pullup/downs.
+  // No need to keep that power domain explicitly, unlike EXT1.
+  rtc_gpio_pullup_dis(WAKEUP_GPIO);
+  rtc_gpio_pulldown_en(WAKEUP_GPIO);
+
+#else  // EXT1 WAKEUP
+  //If you were to use ext1, you would use it like
+  esp_sleep_enable_ext1_wakeup_io(BUTTON_PIN_BITMASK(WAKEUP_GPIO), ESP_EXT1_WAKEUP_ANY_HIGH);
+
+  //  If there are no external pull-up/downs, tie wakeup pins to inactive level with internal pull-up/downs via RTC IO
+   //      during deepsleep. However, RTC IO relies on the RTC_PERIPH power domain. Keeping this power domain on will
+   //      increase some power consumption. However, if we turn off the RTC_PERIPH domain or if certain chips lack the RTC_PERIPH
+  //       domain, we will use the HOLD feature to maintain the pull-up and pull-down on the pins during sleep.
+
+  rtc_gpio_pulldown_en(WAKEUP_GPIO);  // GPIO33 is tie to GND in order to wake up in HIGH
+  rtc_gpio_pullup_dis(WAKEUP_GPIO);   // Disable PULL_UP in order to allow it to wakeup on HIGH
+#endif
+*/
+  //Go to sleep now
+  Serial.println("Going to sleep now");
+  esp_deep_sleep_start();
+  Serial.println("This will never be printed");
+
+}
+void Scanner ()
+{
+  Serial.println ();
+  Serial.println ("I2C scanner. Scanning ...");
+  byte count = 0;
+
+  //Wire.begin();
+    Wire.begin (SDA, SCL);   // sda= GPIO_18 /scl= GPIO_19
+  for (byte i = 8; i < 120; i++)
+  {
+    Wire.beginTransmission (i);          // Begin I2C transmission Address (i)
+    if (Wire.endTransmission () == 0)  // Receive 0 = success (ACK response)
+    {
+      Serial.print ("Found address: ");
+      Serial.print (i, DEC);
+      Serial.print (" (0x");
+      Serial.print (i, HEX);     // PCF8574 7 bit address
+      Serial.println (")");
+      count++;
+    }
+  }
+  Serial.print ("Found ");
+  Serial.print (count, DEC);        // numbers of devices
+  Serial.println (" device(s).");
+}
+
+
+
+
+
