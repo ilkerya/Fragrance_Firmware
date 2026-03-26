4dff1a4f3171018f6bec239982fab7ed04412e56
ilker yagci <ilkerya@gmail.com>
2026-03-26 17:56:28 +0100

    Fixes 26.03.2026_02

--- MemSave.h
+++ MemSave.h
@@ -73,6 +73,7 @@ void Execute_Serial_Commands(void){
           System.RxSuccess = ON;   
       }
        if (receivedMessage.substring(0,4) == "Key") {  //(0,9) == "ColorHigh")  (10,14))
+         Color.Fade = OFF;
           Key_Mode_Update();
           System.RxSuccess = ON;   
       }
